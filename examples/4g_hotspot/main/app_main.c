70e4a02443da0fd8ed1d139a314f28a5dab87cb5
alitekin3fx <alitekin3fx@gmail.com>
2025-01-27 22:25:39 +0300

    test

--- examples/4g_hotspot/main/app_main.c
+++ examples/4g_hotspot/main/app_main.c
@@ -10,6 +10,7 @@
 #include "esp_netif.h"
 #include "esp_event.h"
 #include "esp_bridge.h"
+#include "driver/gpio.h"
 
 static esp_err_t esp_storage_init(void)
 {
@@ -25,15 +26,26 @@ static esp_err_t esp_storage_init(void)
     return ret;
 }
 
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName)
+{
+	printf("%s\n", pcTaskName);
+}
+
+volatile bool test = true;
 void app_main(void)
 {
     esp_storage_init();
-
+    while(test)
+    	vTaskDelay(100);
+    
     /* Init and register system/core components */
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     esp_bridge_create_all_netif();
+
+    gpio_set_direction(GPIO_NUM_32, GPIO_MODE_OUTPUT);
+    ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_32, 1));
 
 #if defined(CONFIG_BRIDGE_DATA_FORWARDING_NETIF_SOFTAP)
     wifi_config_t wifi_cfg = {
